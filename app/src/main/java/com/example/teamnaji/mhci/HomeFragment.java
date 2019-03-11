39946633b21cec44fd86f3e33b801870c024b3e2
Dan Cristian Cecoi <dancristian.cecoi@gmail.com>
2019-03-11 16:21:53 +0000

    change the colours of the home page and the card icons

--- app/src/main/java/com/example/teamnaji/mhci/HomeFragment.java
+++ app/src/main/java/com/example/teamnaji/mhci/HomeFragment.java
@@ -1,9 +1,7 @@
 package com.example.teamnaji.mhci;
 
 import android.os.Bundle;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
